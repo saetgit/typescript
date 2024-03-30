19e8bf6cb286efcdaa6c07dcaf15f8992af4342d
saetgit <etemadian.sara.1998@gmail.com>
2024-03-30 10:56:45 +0330

    add tsconfig to project

---
+++ public/sandBox.js
@@ -0,0 +1,8 @@
+"use strict";
+const firstName = "sara";
+console.log(firstName);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(input => {
+    console.log(input);
+});
