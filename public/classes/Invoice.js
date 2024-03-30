7cc1dd2b4f2648ddb5fdc054b034e2c557cbd041
saetgit <etemadian.sara.1998@gmail.com>
2024-03-30 14:55:15 +0330

    use mixed interface and class

--- public/classes/Invoice.js
+++ public/classes/Invoice.js
@@ -1,13 +1,10 @@
 export class Invoice {
-    // public client: string;
-    // private details: string;
-    // readonly amount: number;
     constructor(client, details, amount) {
         this.client = client;
         this.details = details;
         this.amount = amount;
     }
     format() {
-        return `${this.client} owes ${this.amount} for ${this.details}`;
+        return `${this.client} - ${this.amount} تومان بدهکار برای ${this.details} `;
     }
 }
