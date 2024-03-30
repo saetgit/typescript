8943c5a81255eb0c2fd982e323ad7d117dbecd27
saetgit <etemadian.sara.1998@gmail.com>
2024-03-30 14:11:39 +0330

    add function for button

--- public/sandBox.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const firstName = "sara";
-console.log(firstName);
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(input => {
-    console.log(input);
-});
